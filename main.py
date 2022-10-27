66a6d1debbbc6926684485ebba15fc5bd3eeb4c3
Jurgen Mane <jurgenmane@gmail.com>
2022-10-26 20:30:05 -0500

    Fix issue #7: Bugs when navigating from update to home screen fixed

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def create_frame(window):
     return frame
 
 def draw_emp(frame, employees):
-    ttk.Label(frame, text="ID", borderwidth=2, relief="solid", width=15).grid(column=0, row=2, padx=2)
-    ttk.Label(frame, text="NAME", borderwidth=2, relief="solid", width=15).grid(column=1, row=2, padx=2)
-    ttk.Label(frame, text="SURNAME", borderwidth=2, relief="solid", width=15).grid(column=2, row=2, padx=2)
-    ttk.Label(frame, text="DEPARTMENT", borderwidth=2, relief="solid", width=15).grid(column=3, row=2, padx=2)
-    ttk.Label(frame, text="START DATE", borderwidth=2, relief="solid", width=15).grid(column=4, row=2, padx=2)
+    # ttk.Label(frame, text="ID", borderwidth=2, relief="solid", width=15).grid(column=0, row=2, padx=2)
+    # ttk.Label(frame, text="NAME", borderwidth=2, relief="solid", width=15).grid(column=1, row=2, padx=2)
+    # ttk.Label(frame, text="SURNAME", borderwidth=2, relief="solid", width=15).grid(column=2, row=2, padx=2)
+    # ttk.Label(frame, text="DEPARTMENT", borderwidth=2, relief="solid", width=15).grid(column=3, row=2, padx=2)
+    # ttk.Label(frame, text="START DATE", borderwidth=2, relief="solid", width=15).grid(column=4, row=2, padx=2)
 
 
     row_number = 3
@@ -53,112 +53,109 @@ def update_employee(id, first_name, last_name, department, start_date):
         writer.writerow(employee)
 
 
-def draw_frame(window, employees):
+def draw_parent_frame(window, btn_handlers):
     global parent_frame
-    global child_frame
     global e1
     global e2
 
     parent_frame = create_frame(window)
-    child_frame = create_frame(parent_frame)
-    child_frame.grid(row=3, column=0, columnspan=5, padx=15, pady=15)
 
     e1 = ttk.Entry(parent_frame, width=15)  #Putting the entry inside the frame
     e1.grid(row=0, column=0, padx=2, pady=15)
 
     e2 = ttk.Entry(parent_frame, width=15)  #Putting the entry inside the frame for deleting
     e2.grid(row=0, column=3, padx=2, pady=15)
-    
 
-    def redraw_frame(employees):
-        global child_frame
-        global parent_frame
-        global e1
-        global e2
+
+    btn_1 = ttk.Button(parent_frame, text="Search by Department", command=btn_handlers["search"]).grid(row=0, column=1, padx=15, pady=15)
+    btn_2 = ttk.Button(parent_frame, text="Delete Employee", command=btn_handlers["delete"]).grid(row=0, column=4, padx=15, pady=15)
+    btn_home = ttk.Button(parent_frame, text="HOME", command=btn_handlers["home"]).grid(row=0, column=2, padx=15, pady=15)
+    btn_update = ttk.Button(parent_frame, text="Update", command=btn_handlers["update"]).grid(row=1, column=0, padx=15, pady=15)
+
+    return parent_frame
+
+
+def draw_child_frame(employees):
+    global child_frame
+    global parent_frame
+
+    if child_frame != None:
         child_frame.destroy() #Updating the UI
         child_frame.pack_forget()
-        child_frame = create_frame(parent_frame)
-        child_frame.grid(row=2, column=0, columnspan=5, padx=15, pady=15)
 
-        e1.delete(0, END)
-        e2.delete(0, END)
-
-
-    def btn_search():
-        input_value = e1.get()
-        result = filter_by_department(input_value)
-
-        redraw_frame(result)
-
-
-
-    def btn_delete():
-        input_value = e2.get()
-        employees = read_csv(FILE_PATH)
-        file = open(FILE_PATH, "w")
-        writer = csv.writer(file, delimiter=",")
-        for employee in employees:
-            if employee[0] != input_value:
-                writer.writerow(employee)
-
-        employees = read_csv(FILE_PATH)
-        redraw_frame(employees[1: 10])
-
-    def button_home():
-        employees = read_csv(FILE_PATH)
-        redraw_frame(employees[1:10])
-
-    def button_update():
-        global parent_frame
-        global child_frame
-        
-        child_frame.destroy() #Updating the UI
-        child_frame.pack_forget()
-        child_frame = create_frame(parent_frame)
-        child_frame.grid(row=2, column=0, columnspan=5, padx=15, pady=15)
-        
-        ttk.Label(child_frame, text="ID", borderwidth=2, relief="solid", width=15).grid(column=0, row=0, padx=2)
-        ttk.Label(child_frame, text="NAME", borderwidth=2, relief="solid", width=15).grid(column=1, row=0, padx=2)
-        ttk.Label(child_frame, text="SURNAME", borderwidth=2, relief="solid", width=15).grid(column=2, row=0, padx=2)
-        ttk.Label(child_frame, text="DEPARTMENT", borderwidth=2, relief="solid", width=15).grid(column=3, row=0, padx=2)
-        ttk.Label(child_frame, text="START DATE", borderwidth=2, relief="solid", width=15).grid(column=4, row=0, padx=2)
-
-        e_id = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
-        e_id.grid(row=1, column=0, padx=2, pady=15)
-
-        e_first_name = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
-        e_first_name.grid(row=1, column=1, padx=2, pady=15)
-
-        e_last_name = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
-        e_last_name.grid(row=1, column=2, padx=2, pady=15)
-
-        e_department = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
-        e_department.grid(row=1, column=3, padx=2, pady=15)
-
-        e_start_date = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
-        e_start_date.grid(row=1, column=4, padx=2, pady=15)
-
-        def btn_submit():
-            id = e_id.get()
-            first_name = e_first_name.get()
-            last_name = e_last_name.get()
-            department = e_department.get()
-            start_date = e_start_date.get()
-
-            update_employee(id, first_name, last_name, department, start_date)
-
-
-        btn_submit = ttk.Button(child_frame, text="Submit", command=btn_submit).grid(row=2, column=4, padx=15, pady=15)
-
-
-
-    btn_1 = ttk.Button(parent_frame, text="Search", command=btn_search).grid(row=0, column=1, padx=15, pady=15)
-    btn_2 = ttk.Button(parent_frame, text="Delete Employee", command=btn_delete).grid(row=0, column=4, padx=15, pady=15)
-    btn_home = ttk.Button(parent_frame, text="HOME", command=button_home).grid(row=0, column=2, padx=15, pady=15)
-    btn_update = ttk.Button(parent_frame, text="Update", command=button_update).grid(row=1, column=0, padx=15, pady=15)
-
-    
+    child_frame = create_frame(parent_frame)
+    child_frame.grid(row=2, column=0, columnspan=5, padx=15, pady=15)
     draw_emp(child_frame, employees)
+    
+
+def btn_search():
+    input_value = e1.get()
+    result = filter_by_department(input_value)
+    if len(result) != 0:
+        draw_child_frame(result)
+    else:
+        draw_child_frame([])
+        ttk.Label(child_frame, text="No Result Found", borderwidth=2, relief="solid", width=15).grid(column=0, row=0, padx=2) 
+
+def btn_delete():
+    input_value = e2.get()
+    employees = read_csv(FILE_PATH)
+    file = open(FILE_PATH, "w")
+    writer = csv.writer(file, delimiter=",")
+    for employee in employees:
+        if employee[0] != input_value:
+            writer.writerow(employee)
+
+    employees = read_csv(FILE_PATH)
+    draw_child_frame(employees[0: 10])
+
+def btn_home():
+    employees = read_csv(FILE_PATH)
+    draw_child_frame(employees[0:10])
+
+def btn_update():
+    global parent_frame
+    global child_frame
+    
+    child_frame.destroy() #Updating the UI
+    child_frame.pack_forget()
+    child_frame = create_frame(parent_frame)
+    child_frame.grid(row=2, column=0, columnspan=5, padx=15, pady=15)
+    
+    ttk.Label(child_frame, text="ID", borderwidth=2, relief="solid", width=15).grid(column=0, row=0, padx=2)
+    ttk.Label(child_frame, text="NAME", borderwidth=2, relief="solid", width=15).grid(column=1, row=0, padx=2)
+    ttk.Label(child_frame, text="SURNAME", borderwidth=2, relief="solid", width=15).grid(column=2, row=0, padx=2)
+    ttk.Label(child_frame, text="DEPARTMENT", borderwidth=2, relief="solid", width=15).grid(column=3, row=0, padx=2)
+    ttk.Label(child_frame, text="START DATE", borderwidth=2, relief="solid", width=15).grid(column=4, row=0, padx=2)
+
+    e_id = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
+    e_id.grid(row=1, column=0, padx=2, pady=15)
+
+    e_first_name = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
+    e_first_name.grid(row=1, column=1, padx=2, pady=15)
+
+    e_last_name = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
+    e_last_name.grid(row=1, column=2, padx=2, pady=15)
+
+    e_department = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
+    e_department.grid(row=1, column=3, padx=2, pady=15)
+
+    e_start_date = ttk.Entry(child_frame, width=15)  #Putting the entry inside the frame for updating
+    e_start_date.grid(row=1, column=4, padx=2, pady=15)
+
+
+    def btn_submit():
+        id = e_id.get()
+        first_name = e_first_name.get()
+        last_name = e_last_name.get()
+        department = e_department.get()
+        start_date = e_start_date.get()
+
+        update_employee(id, first_name, last_name, department, start_date)
+        employees = read_csv(FILE_PATH)
+        draw_child_frame(employees[1: 10])
+
+    btn_submit = ttk.Button(child_frame, text="Submit", command=btn_submit).grid(row=2, column=4, padx=15, pady=15)
 
 
 
@@ -171,14 +168,22 @@ def filter_by_department(dpt):
 
     return result    
 
+
 def main():
         
     window = tkinter.Tk()  #Creating the window
     window.geometry("900x900")
 
     employees = read_csv(FILE_PATH)
+    handlers = {
+        "home": btn_home,
+        "search": btn_search,
+        "update": btn_update,
+        "delete": btn_delete
+    }
 
-    draw_frame(window, employees[1:10])
+    draw_parent_frame(window, handlers)
+    draw_child_frame(employees[0: 10])
 
     window.mainloop()
 
